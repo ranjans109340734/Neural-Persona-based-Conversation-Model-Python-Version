9e1e51a1c6d5c571de38505150bdac572e83975e
Amit Hulandageri <36067915+amithm92@users.noreply.github.com>
2020-03-21 16:55:34 -0600

    Update params.py

--- params.py
+++ params.py
@@ -7,16 +7,19 @@ class params:
 		self.layers=4
 		self.SpeakerNum=2
 		self.PersonaMode=True
+		
 		self.train_path="data/testing"
 		self.train_file="/train.txt"
 		self.dev_file="/valid.txt"
 		self.dictPath="/vocabulary"
+		
 		self.fine_tuning=False
 		self.fine_tuning_model="save/model"
 		if self.PersonaMode:
 			self.saveFolder="save/"+self.train_path.split("/")[-1]
 		else:
 			self.saveFolder="save/"+self.train_path.split("/")[-1]+"/non_persona"
+			
 		self.init_weight=0.1
 		self.alpha=1
 		self.start_halve=6
@@ -27,9 +30,11 @@ class params:
 		self.thres=5
 		self.source_max_length=50
 		self.target_max_length=50
+		
 		self.save_prefix=self.saveFolder+"/model"
 		self.save_params_file=self.saveFolder+"/params"
 		self.output_file=self.saveFolder+"/log"
+		
 		self.reverse=False
 		self.reverse_target=False
 		self.saveModel=True
