78af23a5bf5a82e87e08028b944ebc73a21bf80e
Amit Hulandageri <36067915+amithm92@users.noreply.github.com>
2020-03-23 00:47:27 -0600

    Update persona.py

--- persona.py
+++ persona.py
@@ -273,7 +273,8 @@ class persona:
             print(len(output))
             for i in range(0,8):
                 print(output[i].size())
-            print("LSTM",lstm_source.parameters()[1].size())
+                print("LSTM",self.lstm_source.parameters()[i+1].size())
+            
             
             if t==self.Word_s.size(1)-1:
                 self.last=output
