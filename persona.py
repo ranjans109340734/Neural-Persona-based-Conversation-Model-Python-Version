f94e4afbaa43d8052fda2ca0197476280ace3aa1
Amit Hulandageri <36067915+amithm92@users.noreply.github.com>
2020-03-24 00:17:37 -0600

    Update persona.py

--- persona.py
+++ persona.py
@@ -82,10 +82,10 @@ class lstm_source_(nn.Module):
             # Input data(mini-batch) for each layer in 512 dimension
             reshaped_gates=gates.view(-1,4,self.params.dimension)       #256*4*512
             
-            print(reshaped_gates[:,0])
-            print(reshaped_gates[:,1])
-            print(reshaped_gates[:,2])
-            print(reshaped_gates[:,3])
+            print(reshaped_gates[:,0].size())
+            print(reshaped_gates[:,1].size())
+            print(reshaped_gates[:,2].size())
+            print(reshaped_gates[:,3].size())
             
             #forget gate
             forget_gate= nn.Sigmoid()(reshaped_gates[:,2])      #256*512
