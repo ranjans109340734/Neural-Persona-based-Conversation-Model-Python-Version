29926f2cbb7ab76ed9db7ec8e8fd376517b3da3a
Amit Hulandageri <36067915+amithm92@users.noreply.github.com>
2020-03-25 21:24:56 -0600

    Update persona.py

--- persona.py
+++ persona.py
@@ -459,6 +459,8 @@ class persona:
 
     def save(self):
         #syntax: torch.save(the_model.state_dict(), PATH)
+        #PATH: save/testing/model_x_xxxxx.pkl or save/testing/non_persona/model_x_xxxxx.pkl
+        
         torch.save(self.lstm_source.state_dict(),self.params.save_prefix+str(self.iter)+"_source.pkl")
         torch.save(self.lstm_target.state_dict(),self.params.save_prefix+str(self.iter)+"_target.pkl")
         torch.save(self.softmax.state_dict(),self.params.save_prefix+str(self.iter)+"_softmax.pkl")
